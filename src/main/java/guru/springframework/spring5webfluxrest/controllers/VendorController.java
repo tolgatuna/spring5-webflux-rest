1de2a0f05488d3c282a2b50f649058a6fd16c719
tolgatuna <tolga.tuna@etstur.com>
2018-10-12 00:07:45 +0300

    Assignment Completed...

--- src/main/java/guru/springframework/spring5webfluxrest/controllers/VendorController.java
+++ src/main/java/guru/springframework/spring5webfluxrest/controllers/VendorController.java
@@ -3,9 +3,9 @@ package guru.springframework.spring5webfluxrest.controllers;
 
 import guru.springframework.spring5webfluxrest.domain.Vendor;
 import guru.springframework.spring5webfluxrest.repositories.VendorRepository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.reactivestreams.Publisher;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
@@ -13,6 +13,7 @@ import reactor.core.publisher.Mono;
  * Created by jt on 12/30/17.
  */
 @RestController
+@RequestMapping("/api/v1/vendors")
 public class VendorController {
 
     private final VendorRepository vendorRepository;
@@ -21,13 +22,24 @@ public class VendorController {
         this.vendorRepository = vendorRepository;
     }
 
-    @GetMapping("/api/v1/vendors")
+    @GetMapping()
     Flux<Vendor> list(){
         return vendorRepository.findAll();
     }
 
-    @GetMapping("api/v1/vendors/{id}")
+    @GetMapping("/{id}")
     Mono<Vendor> getById(@PathVariable String id){
         return vendorRepository.findById(id);
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping()
+    Mono<Void> create(@RequestBody Publisher<Vendor> vendorPublisher) {
+        return vendorRepository.saveAll(vendorPublisher).then();
+    }
+
+    @PutMapping("/{id}")
+    Mono<Vendor> crate(@PathVariable String id, @RequestBody Vendor vendorMono) {
+        return vendorRepository.save(vendorMono);
     }
 }
--- src/test/java/guru/springframework/spring5webfluxrest/controllers/VendorControllerTest.java
+++ src/test/java/guru/springframework/spring5webfluxrest/controllers/VendorControllerTest.java
@@ -6,9 +6,12 @@ import org.junit.Before;
 import org.junit.Test;
 import org.mockito.BDDMockito;
 import org.mockito.Mockito;
+import org.reactivestreams.Publisher;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import static org.mockito.ArgumentMatchers.any;
 
 public class VendorControllerTest {
 
@@ -47,4 +50,37 @@ public class VendorControllerTest {
                 .exchange()
                 .expectBody(Vendor.class);
     }
+
+    @Test
+    public void create() {
+        BDDMockito.given(vendorRepository.saveAll(any(Publisher.class)))
+                .willReturn(Flux.just(Vendor.builder().build()));
+
+        Vendor vendor = Vendor.builder().firstName("Test").lastName("TEST").build();
+        webTestClient.post()
+                .uri("/api/v1/vendors")
+                .body(Mono.just(vendor), Vendor.class)
+                .exchange()
+                .expectStatus()
+                .isCreated();
+
+    }
+
+    @Test
+    public void update() {
+        BDDMockito.given(vendorRepository.save(any(Vendor.class)))
+                .willReturn(Mono.just(Vendor.builder().firstName("Test").lastName("TEST").build()));
+
+        Vendor vendor = Vendor.builder().firstName("Test").lastName("TEST").build();
+
+        webTestClient.post()
+                .uri("/api/v1/vendors/asdsada")
+                .body(Mono.just(vendor), Vendor.class)
+                .exchange()
+                .expectStatus()
+                .isOk();
+
+    }
+
+
 }
